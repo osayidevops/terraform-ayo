a3f646f2592b5af03a2496c20b43be64cd521d7c
kelvingit <kelvinenofedevops@gmail.com>
2026-04-20 03:34:16 +0100

    fix: move ssh key into repo for CI/CD compatibility

--- main.tf
+++ main.tf
@@ -50,7 +50,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
 
   admin_ssh_key {
     username   = "azureuser"
-    public_key = file("~/.ssh/id_rsa.pub")
+    public_key = file("${path.module}/ssh_key.pub")
   }
 
   os_disk {
