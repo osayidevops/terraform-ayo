4dd9035b5eb19457d99a372c0137355a95cbdf7f
osayidevops <osayidevops@gmail.com>
2026-04-21 12:17:56 +0100

    Refactor Ansible structure to production layout + fix Terraform NSG updates

--- main.tf
+++ main.tf
@@ -71,6 +71,7 @@ resource "azurerm_network_security_group" "nsg" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
+  # SSH access
   security_rule {
     name                       = "allow-ssh"
     priority                   = 1000
@@ -79,6 +80,19 @@ resource "azurerm_network_security_group" "nsg" {
     protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  # HTTP access (NGINX)
+  security_rule {
+    name                       = "allow-http"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
